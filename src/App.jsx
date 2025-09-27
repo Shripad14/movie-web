be5397b9d5264f081899482d29fdc2ac6f4388ca
Shripad Dhanshetti <shripad1483@gmail.com>
2025-09-28 00:23:11 +0530

    feat(NavBar.jsx): Added Navbar.jsx. Navigation Bar w/o css. fix(App.jsx): Called the navbar component in App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,20 @@ import MovieCard from './components/MovieCard';
 import Home from './pages/Home';
 import { Routes, Route } from 'react-router-dom';
 import Favourites from './pages/Favourites';
+import NavBar from './components/NavBar';
+
 const App = () => {
    
   return (
-  <main className='main-content'>
+  <div>
+    <NavBar/>
+    <main className='main-content'>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/favorites' element={<Favourites/>}/>
     </Routes>
   </main>
+  </div>
 )}
 
 export default App
